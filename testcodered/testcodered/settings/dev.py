fe2be4d2f46624c0666ce8bab1e82e8f04c86018
BenedictusAryo <benedictusaryo@outlook.com>
2021-02-21 21:44:33 +0700

    test codered

---
+++ testcodered/testcodered/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'zg_q(uwifk4u-ilrglbyk9d_ey4aed4n7rg+5n4cuku0x7dllb'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
